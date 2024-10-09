c7ec4f887ebbeeb39e26d4e4e977a35ec50b8a7b
Ibrahim Goumrane <goumrane.ibrahim@ensam-casa.ma>
2024-10-09 13:58:04 +0100

    `chore: Update npm dependency to latest stable version`

--- src/Settings/Settings.ts
+++ src/Settings/Settings.ts
@@ -1,4 +1,4 @@
-const LOCAL_ADDRESS = "http://192.168.1.9:5010";
+const LOCAL_ADDRESS = "http://localhost:5010";
 const PRODUCTION_ADDRESS = "Production url of pur server";
 export const devEnv = "development";
 export const serverAddress = devEnv ? LOCAL_ADDRESS : PRODUCTION_ADDRESS;
--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/uploads/, "/uploads"),
       },
     },
-    host: "0.0.0.0", // Listen on all interfaces
+    host: "localhost", // Listen on all interfaces
     port: 5173,
   },
 });
